0d713ef604bb00f884147cfb1e629e90a35d4d97
Leila Sol Mattone <leilarock@MacBook-Air-de-Leila.local>
2020-01-15 21:43:06 -0300

    Colocamos numeros a la grilla

--- js/app.js
+++ js/app.js
@@ -4,6 +4,11 @@
 var TableroDef = new Tablero();
 TableroDef.GenerarGrilla();
 
+
+
+
+
+// MOSTRAR TABLERO
 var tablero = document.getElementById('Tablero');
 
 for (var i = 0; i < 10; i++) {
@@ -11,11 +16,20 @@ for (var i = 0; i < 10; i++) {
     for (var j = 0; j < 10; j++) {
 
         var casillero = document.createElement("div");
+        if (TableroDef.casilleros[i][j].mina == true) {
+            casillero.style.backgroundColor = 'yellow';
+        } 
+        if(TableroDef.casilleros[i][j].numero >0){
+            casillero.innerHTML = TableroDef.casilleros[i][j].numero;
+
+        }
         casillero.className = "grillaCompleta";
         //casillero.textContent = "l"+(i+1) + " - c" + (j+1);
         tablero.appendChild(casillero);
     }
 }
+
+
 
 
 
--- js/tablero.js
+++ js/tablero.js
@@ -4,8 +4,8 @@ var Tablero = function () {
 
 
 var mostrarArray = function (array) {
-    for (i = 0; i < array.length; i++) {
-        for (j = 0; j < array[i].length; j++) {
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < array[i].length; j++) {
             console.log(array[i][j]);
         }
     }
@@ -14,17 +14,18 @@ var mostrarArray = function (array) {
 
 Tablero.prototype.GenerarGrilla = function() {
     var array = [];
-    for (i = 0; i < 10; i++) {
+    for (var i = 0; i < 10; i++) {
         array[i] = [];
-        for (j = 0; j < 10; j++) {
+        for (var j = 0; j < 10; j++) {
             array[i][j] = new Casilla(false, 0, true);
         }
     }
     this.casilleros = array; 
     this.GenerarMinas();
+    this.GenerarNumeros();
 }
 
-Tablero.prototype.GenerarMinas = function(){ //FALTA TERMINARLA
+Tablero.prototype.GenerarMinas = function(){ 
     // como convertir una casilla en una mina manualmente this.casilleros[0][0].mina = true;
 
    /*Opcion hecha con el for para entender for (var i = 0; i < 10; i++){
@@ -39,7 +40,53 @@ Tablero.prototype.GenerarMinas = function(){ //FALTA TERMINARLA
         if (this.casilleros[randomFila][randomColumna].mina == false) {
         this.casilleros[randomFila][randomColumna].mina = true;
         minas -= 1;
+    
         }
     }
     console.log(this.casilleros);
+    
 }
+
+Tablero.prototype.GenerarNumeros = function () {
+
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            this.casilleros[i][j].numero = this.obtenerNumero(i, j);
+
+
+        }
+    }
+}
+
+Tablero.prototype.obtenerNumero = function (k, m) {
+    var cantidadMinasAlrededor = 0;
+    var filaDesde = k-1;
+    var filaHasta = k+1;
+    var columnaDesde = m-1;
+    var columnaHasta = m+1;
+    
+    if(filaDesde<=0){
+        filaDesde=0;
+    }
+    if (filaHasta >= 9) {
+        filaHasta = 9;
+    }
+
+    if (columnaDesde <= 0) {
+        columnaDesde = 0;
+    }
+    if (columnaHasta >= 9) {
+        columnaHasta = 9;
+    }
+
+    for (var i = filaDesde; i <= filaHasta; i++) {// recorro la fila
+        for (var j = columnaDesde; j <= columnaHasta; j++) {// recorro la columna alrededor de la posicion recibida
+            if(this.casilleros[i][j].mina){
+                cantidadMinasAlrededor++;
+            }
+
+        }
+    }
+    return cantidadMinasAlrededor;
+}
+
